16a27318fad23d458a5c2c04d952096126fbc1bd
Gentle <cyc07297@gmail.com>
2017-08-12 20:52:24 +0900

    no message

---
+++ 13th_AppJam/Assets/Script/HpBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HpBar : MonoBehaviour {
+    private CharacterSheet characterSheet;
+    private CharacterData characterData;
+
+    private Image image;
+
+	// Use this for initialization
+	void Start () {
+        characterData = characterSheet.m_data[0];
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        image.fillAmount = characterData.hp / 100;
+    }
+}
