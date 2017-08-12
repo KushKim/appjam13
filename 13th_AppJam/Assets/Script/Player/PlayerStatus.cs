b38ce608c8d20dfaa13406bbb02eb8666c02e2c1
HyunDo_Lee <godd7776@gmail.com>
2017-08-13 01:21:26 +0900

    [ Create ] (Sprite) Icon, Title (Script) Director

--- 13th_AppJam/Assets/Script/Player/PlayerStatus.cs
+++ 13th_AppJam/Assets/Script/Player/PlayerStatus.cs
@@ -4,12 +4,5 @@ using UnityEngine;
 
 public class PlayerStatus : MonoBehaviour
 {
-<<<<<<< HEAD
-    //public float Hp
-    //{
-    //    
-    //}
-=======
 
->>>>>>> 73c57308a9c4be1f400b9686e8af89c9dde498a8
 }
