ef8792801020d5b4448a384ef57a1d9ecbfb3088
HyunDo_Lee <godd7776@gmail.com>
2017-08-13 02:31:06 +0900

    [ Create ] Title Map, Loading Map, Loading
    [ Fixed ]
    HpBar, PlayerStatus

--- 13th_AppJam/Assets/Script/Player/PlayerStatus.cs
+++ 13th_AppJam/Assets/Script/Player/PlayerStatus.cs
@@ -4,8 +4,55 @@ using UnityEngine;
 
 public class PlayerStatus : MonoBehaviour
 {
-    //public float Hp
-    //{
-    //    
-    //}
+    [SerializeField]
+    private HpBar hpBar;
+
+    private float hp;
+    private int star;
+ 
+    private PlayerDataContainer playerContainer;
+
+    private CharacterSheet characterSheet;
+    private CharacterData characterData;
+
+    public float Hp
+    {
+        set
+        {
+            hp = value;
+            hpBar.UpdateHpBar(hp);
+        }
+        get
+        {
+            return hp;
+        }
+    }
+    public int Star
+    {
+        set
+        {
+            star = value;
+        }
+        get
+        {
+            return star;
+        }
+    }
+
+    private void Awake()
+    {
+        playerContainer = InGameManager.Instance._PlayerDataContainer;
+
+        characterSheet = InGameManager.Instance.CharacterSheet_readonly;
+
+        for(int i = 0; i < characterSheet.m_data.Count; i++)
+        {
+            if(characterSheet.m_data[i].name == playerContainer.CharacterName)
+                characterData = characterSheet.m_data[i];
+        }
+
+        Hp = characterData.hp;
+
+        star = 0;
+    }
 }
