9c2bfbfb41e125449de4950770e4c3d4e236bbc7
HyunDo_Lee <godd7776@gmail.com>
2017-08-12 16:02:16 +0900

    [ Create ]

---
+++ 13th_AppJam/Assets/Script/SceneChanger.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    private static SceneChanger instance = null;
+    public static SceneChanger Instance
+    {
+        get
+        {
+            if (instance)
+                return instance;
+            else
+                return instance = new GameObject("SceneChanger").AddComponent<SceneChanger>();
+        }
+    }
+
+    public string SceneName { set; get; }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneName = sceneName;
+
+        SceneManager.LoadScene("Load");
+    }
+}
+
+public struct SceneType
+{
+    public static string Title = "Title";
+    public static string Menu = "Menu";
+    public static string InGame = "InGame";
+}
